8876480732f64d96a6f1edb9c6b5a37ba86e403e
oxalica <oxalicc@pm.me>
2024-09-21 08:05:58 -0400

    test: add method to dump query plans

--- blahd/src/database/tests.rs
+++ blahd/src/database/tests.rs
@@ -1,5 +1,11 @@
 #![expect(clippy::print_stdout, reason = "allowed in tests for debugging")]
+use std::fmt::Write;
+use std::fs;
+use std::process::{Command, Stdio};
+
 use super::*;
+
+const SRC_PATH: &str = "src/database.rs";
 
 #[test]
 fn init_sql_valid() {
@@ -20,7 +26,7 @@ fn init_sql_valid() {
 
 #[test]
 fn stmt_cache_capacity() {
-    let src = std::fs::read_to_string("src/database.rs").unwrap();
+    let src = fs::read_to_string(SRC_PATH).unwrap();
     let sql_cnt = src.matches("prepare_cached_and_bind!").count();
     println!("found {sql_cnt} SQLs");
     assert_ne!(sql_cnt, 0);
@@ -28,4 +34,32 @@ fn stmt_cache_capacity() {
         sql_cnt <= STMT_CACHE_CAPACITY,
         "stmt cache capacity {STMT_CACHE_CAPACITY} is too small, found {sql_cnt} SQLs",
     );
+}
+
+#[test]
+#[ignore = "only for debugging"]
+fn dump_query_plan() {
+    let src = fs::read_to_string(SRC_PATH).unwrap();
+    let mut cmds = String::new();
+    for (pos, _) in src.match_indices("prepare_cached_and_bind!") {
+        let line = src[..pos].matches('\n').count() + 1;
+        let sql = src[pos..]
+            .lines()
+            // Skip macro call, first argument, `r"`.
+            .skip(3)
+            .take_while(|line| line.trim() != "\"")
+            .flat_map(|line| [line.trim(), "\n"])
+            .collect::<String>();
+        writeln!(cmds, "SELECT '{SRC_PATH}:{line}';").unwrap();
+        writeln!(cmds, "EXPLAIN QUERY PLAN {sql};").unwrap();
+    }
+
+    let st = Command::new("sqlite3")
+        .stdin(Stdio::null())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .args([":memory:", ".read ./schema.sql", &cmds])
+        .status()
+        .unwrap();
+    assert!(st.success());
 }
